b505cddcf1c7c2776d3f623e3a50de300c040c73
Shravan Reddy <shravan@cs.stanford.edu>
2014-06-24 13:12:36 -0700

    add author and maintainer info

--- setup.py
+++ setup.py
@@ -10,6 +10,10 @@ import process
 setup(
   name='process',
   version=process.__version__,
+  author='Shravan Reddy',
+  author_email='shravan@pixelapse.com',
+  maintainer='Pixelapse',
+  maintainer_email='hello@pixelapse.com',
   packages=find_packages(),
   long_description=open('README.md').read(),
   license=open('LICENSE').read()
